9de986dd7cd9dcdc2ccf841e643f87c751e77398
lforet <laird@isotope11.com>
2011-02-01 16:08:34 -0800

    worked on date id

--- find_19.py
+++ find_19.py
@@ -10,7 +10,108 @@ import math, operator
 import time
 import scipy.spatial
 import ImageChops
+import ImageOps
 
+
+def decimal2binary(n):
+    '''convert denary integer n to binary string bStr'''
+    bStr = ''
+    if n < 0:  raise ValueError, "must be a positive integer"
+    if n == 0: return '0'
+    while n > 0:
+        bStr = str(n % 2) + bStr
+        n = n >> 1
+    return bStr
+
+def digitlist(value, numdigits=8, base=2):
+	val = value
+	return_str = ""
+	digits = [0 for i in range(numdigits)]
+	for i in range(numdigits):
+		val, digits[i] = divmod(val, base)
+		return_str = return_str + str(digits[i])
+	print return_str
+	return digits
+
+def is_uniformLBP(digits):
+	a = digits[0]
+	transition_count = 0
+	for i in range(1,len(digits)):
+			if a != digits[i]:
+				transition_count = transition_count + 1
+				a = digits[i]
+	if transition_count < 3:
+		answer = True
+	else: 
+		answer = False
+	return transition_count, answer
+
+
+def image2array(im):
+	#Create array of image using numpy
+    return numpy.asarray(im)
+
+
+def array2image(a):
+#Create image from array
+	return Image.fromarray(a)
+
+
+def CalcLBP(img):
+	#Function to calculate local binary pattern of an image 
+	#pass in a img
+	#returns an lbp histogram
+	# Angle step.
+	#PI = 3.14159265
+	neighbors = 8
+	#a = 2*PI/neighbors;
+	#radius = 1
+	#Increment = 1/neighbors
+	xmax = img.size[0]
+	ymax = img.size[1] 
+	#convert image to grayscale
+	grayimage = ImageOps.grayscale(img)
+	#make a copy to return
+	returnimage = Image.new("L", (xmax,ymax))
+	uniform_hist = numpy.zeros(256)
+	print "uniform_hist = ", uniform_hist
+	print "size = ", uniform_hist.ndim
+	meanRGB = 0
+	imagearray = grayimage.load()
+	neighborRGB = numpy.empty([8], dtype=int)
+	for y in range(1, ymax-1, 1):				
+		for x in range(1, xmax-1, 1):
+			centerRGB = imagearray[x, y]
+			meanRGB = centerRGB
+			neighborRGB[4] = imagearray[x+1,y+1]
+			neighborRGB[5] = imagearray[x,y+1]
+			neighborRGB[6] = imagearray[x-1,y+1]
+			neighborRGB[7] = imagearray[x-1,y]
+			neighborRGB[0] = imagearray[x-1,y-1]
+			neighborRGB[1] = imagearray[x,y-1]
+			neighborRGB[2] = imagearray[x+1,y-1]
+			neighborRGB[3] = imagearray[x+1,y]
+			#comparing against mean adds a sec vs comparing against center pixel
+			meanRGB= centerRGB + neighborRGB.sum()
+			meanRGB = meanRGB / (neighbors+1)
+			#compute Improved local binary pattern (center pixel vs the mean of neighbors)
+			lbp = 0						
+			for i in range(neighbors):
+			#comparing against mean adds a sec vs comparing against center pixel
+				if neighborRGB[i] >= meanRGB:
+				#if neighborRGB[i] >= centerRGB:
+					lbp = lbp + (2**i)
+			#putpixel adds 1 second vs storing to array
+			uniform = is_uniformLBP( digitlist(lbp, numdigits=8, base=2))
+			print "lbp = ", lbp, " bits = ", decimal2binary(lbp), digitlist(lbp, numdigits=8, base=2), " is_uniformLBP(digits) = ", uniform
+			
+			#if uniform[1] == False:
+			uniform_hist[lbp] = uniform_hist[lbp] +1
+			#time.sleep(1)
+			returnimage.putpixel((x,y), lbp)
+	print uniform_hist
+	print returnimage.histogram()
+	return returnimage
 
 def rotate_image(img, degrees):
 	"""
@@ -110,6 +211,34 @@ if __name__=="__main__":
 	best_orientation = (0,0)
 	best_hu = 0
 	hu2 = numpy.array(cv.GetHuMoments(cv.Moments(img2_copy)))
+
+# - -------- segment the image and build LBP for image
+	segments = 8  
+	xsegs = obj_width  / segments
+	ysegs = obj_height / segments
+	print "xsegs, ysegs = ", xsegs, ysegs 
+	print obj_width % xsegs, obj_height % ysegs
+	for yy in range(0,obj_height-ysegs+1 , ysegs):
+		for xx in range(0,obj_width-xsegs+1,xsegs):
+			#j = raw_input("press any key")
+			#print "xx, yy =", xx, yy, xx+xsegs, yy+ysegs
+			pt1 = (xx, yy)
+			pt2 = (xx+xsegs, yy+ysegs)
+			#cv.Rectangle(img2_copy , pt1, pt2, cv.CV_RGB(255, 255, 255), 1, 0)
+			cropped_img1 = cv.GetSubRect(feature_to_find, (xx, yy,xsegs, ysegs))
+			pil_img1 = Image.fromstring("L", cv.GetSize(cropped_img1), cropped_img1.tostring())
+			lbp_img = CalcLBP(pil_img1)
+			#cv.ShowImage("img2_copy ", img2_copy )
+			#cv.ShowImage("cropped_img1 ", cropped_img1 )
+			cv.WaitKey()
+		    #box = (xx, yy, xx+xsegs, yy+ysegs)
+			#print box
+	#	    cell = PILimg.crop(box)
+
+	#	    CellPixels = list(cell.getdata())
+
+
+
 
 for y in range (230, (img_height-obj_height), 1):
 	for x in range(420,(img_width-obj_width),1):	
